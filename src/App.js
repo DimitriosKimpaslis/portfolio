91dddb6155b38db5cc4be81ba244ba2f4a14769a
DImitrios Kimpaslis <DimitriosKimpaslis@gmail.com>
2024-03-27 19:15:03 +0200

    favicon and loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import Contact from "./components/Contact";
 import IntroContainer from "./components/IntroContainer";
 import Navigation from "./components/Navigation";
 import Projects from "./components/Projects";
+import Loading from "./components/Loading";
 
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const removeLoader = () => {
+      setLoading(false)
+      setTimeout(() => {
+        document.body.style.overflow = "auto";
+      }, 1500)
+    }
+    window.addEventListener('load', removeLoader)
+    return () => window.removeEventListener('load', removeLoader)
+  }
+    , [])
+  
   return (
     <div className="bg-blue-600 flex justify-center py-4 relative">
+      <Loading loading={loading}/>
       <div className="bg-white p-4 lg:w-[90%] w-[98%] rounded-3xl lg:px-16 xl:px-32 2xl:px-60">
         <Navigation />
         <IntroContainer />
---
+++ src/components/Loading.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+const Loading = (props) => {
+    const { loading } = props
+    const [hidden, setHidden] = useState(false)
+    useEffect(() => {
+        if (!loading) {
+            setTimeout(() => setHidden(true), 1500)
+        }
+    }, [loading])
+return (
+    <div className={'w-screen h-screen bg-blue-600 text-2xl font-bold absolute flex justify-center items-center top-0 transition-opacity ' + (loading ? 'opacity-100 ' : 'opacity-0 ') + (hidden && "  hidden")}>
+        <div className={'loader transition-opacity ' + (loading ? 'opacity-100' : 'opacity-0')}></div>
+    </div>
+)
+}
+
+export default Loading
